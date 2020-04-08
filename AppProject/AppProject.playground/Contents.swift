de07c4053b46fed87e4eb56dbafc0635c6e642cc
Armand BLIN <46201207+aygoun@users.noreply.github.com>
2020-04-08 19:07:31 +0200

    Better with struct instead of class

--- AppProject/AppProject.playground/Contents.swift
+++ AppProject/AppProject.playground/Contents.swift
@@ -5,27 +5,26 @@
 //  Created by Armand BLIN on 05/04/2020.
 //
 
-class Party {
+struct party {
+    private let numberOfParty: Int;
     var place: String
     var time: String
     var date: String
     var nbr_people: Int?
     
-    init(place: String, time: String, date: String, nbr_people: Int) {
+    init(numberOfParty: Int, place: String, time: String, date: String, nbr_people: Int) {
+        self.numberOfParty = numberOfParty
         self.place = place
         self.time = time
         self.date = date
         self.nbr_people = nbr_people
     }
-    func CreateParty() {
-        
-    }
 }
 
-var Party1 = Party(place: "Aix", time: "10h20", date: "TEST", nbr_people: 22)
+var Party1 = party(numberOfParty: 3156545, place: "Aix", time: "10h20", date: "TEST", nbr_people: 22)
 Party1.time
 
-class users {
+struct users {
     var first_name: String
     var second_name: String
     var age: Int?
@@ -45,5 +44,18 @@ class users {
     }
 }
 
+
 var UnUser = users(first_name: "Armand", second_name: "BLIN", age: 16, sexe: "Homme", ProfilPic: "TEST", biography: "Un jeune Aixois", password: "Mon MDP")
 UnUser.first_name
+
+struct message {
+    let date_mess: Int
+    let hour_mess: Int
+    let content_mess: String
+    
+    init(date_mess: Int, hour_mess: Int, content_mess: String) {
+        self.date_mess = date_mess
+        self.hour_mess = hour_mess
+        self.content_mess = content_mess
+    }
+}
